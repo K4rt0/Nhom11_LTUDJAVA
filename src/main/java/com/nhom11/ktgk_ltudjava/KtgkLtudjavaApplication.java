d77dec9ff1b675f093862b1632ba60726c2a1d0a
K4rt0 <duongkhoanam147@gmail.com>
2024-06-12 08:46:32 +0700

    first commit

---
+++ src/main/java/com/nhom11/ktgk_ltudjava/KtgkLtudjavaApplication.java
@@ -0,0 +1,13 @@
+package com.nhom11.ktgk_ltudjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtgkLtudjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtgkLtudjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhom11/ktgk_ltudjava/KtgkLtudjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom11.ktgk_ltudjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtgkLtudjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
